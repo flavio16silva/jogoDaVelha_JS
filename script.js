87d8ce5ef7a220d419e6294d472956cf14b065a7
Flavio Silva <fal_silva@hotmail.com>
2024-11-13 09:35:44 -0300

    jogo da velha com JS

---
+++ script.js
@@ -0,0 +1,17 @@
+// ------- Dados Iniciais -----------
+//Criando um objeto com cada casa do jogo
+let tabuleiro = {
+    a1: '', a2: '', a3: '',
+    b1: '', b2: '', b3: '',
+    c1: '', c2: '', c3: '' 
+}
+let vez = ''
+let mensagem = '' //mensagem de vencedor
+let jogo = false  //Identifica se estão jogando ou não após vitoria
+
+
+// --------- Eventos -------------
+
+
+
+// ---------- Funções ------------
